651b09edb1595cd36329bdd264adda790e6e3583
Sahil Siddiki <134954614+siddikisahil47@users.noreply.github.com>
2025-01-29 13:02:28 +0530

    update deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn --bind :$8000 app:app
+web: gunicorn app:app --bind 0.0.0.0:8000 --workers 2 --threads 2 --timeout 60
 
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from api import create_app
 from dotenv import load_dotenv
 
@@ -7,6 +8,5 @@ load_dotenv()
 # Create Flask application
 app = create_app()
 
-if __name__ == '__main__':
-    # Run the application in debug mode
-    app.run(host='0.0.0.0', port=8000, debug=True) 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000) 
